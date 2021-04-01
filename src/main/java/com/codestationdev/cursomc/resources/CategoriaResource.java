98f405ee15e776a337fa1c45fb37a5b7c1d758ee
Pitter roberson <pitterroberson@gmail.com>
2021-04-01 12:01:39 -0300

    criado metodo categoria update

--- src/main/java/com/codestationdev/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/codestationdev/cursomc/resources/CategoriaResource.java
@@ -25,7 +25,7 @@ public class CategoriaResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> findId(@PathVariable Integer id) {
 
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
 	}
@@ -38,4 +38,11 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
 }
